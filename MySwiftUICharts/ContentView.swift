c21814fbf27e148a08726689695ca5352554b536
Mufti Ramdhani <muftiramdhani@gmail.com>
2021-03-07 20:41:44 +0700

    Initial Commit

---
+++ MySwiftUICharts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MySwiftUICharts
+//
+//  Created by Mufti Ramdhani on 07/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MySwiftUICharts/MySwiftUIChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftUIChartsApp.swift
+//  MySwiftUICharts
+//
+//  Created by Mufti Ramdhani on 07/03/21.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftUIChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
